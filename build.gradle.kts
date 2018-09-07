7cb9c4177894cbcb9718c7ffc8c80bc9d1340b10
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2018-09-07 00:56:00 -0400

    Publish a malicious plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,48 @@
+import com.gradle.publish.PublishTask
 
+plugins {
+    java
+    id("com.gradle.plugin-publish") version "0.9.10"
+    id("java-gradle-plugin")
+}
+
+group = "org.jlleitschuh.testing.security"
+version = "0.2.0"
+
+dependencies {
+    compileOnly(gradleApi())
+}
+
+gradlePlugin {
+    (plugins) {
+        "securityPlugin" {
+            id = "org.jlleitschuh.testing.security-plugin"
+            implementationClass = "org.jlleitschuh.testing.security.SecurityPlugin"
+        }
+
+        "securityPluginTemp" {
+            id = "org.jlleitschuh.testing.security-plugin.tmp"
+            implementationClass = "org.jlleitschuh.testing.security.SecurityPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    description = "Useless security testing."
+
+    vcsUrl = "https://github.com/JLLeitschuh/gradle-testing"
+    website = "https://github.com/JLLeitschuh/gradle-testing"
+    tags = listOf("dont-use")
+
+    (plugins) {
+        "securityPlugin" {
+            id = "org.jlleitschuh.testing.security-plugin.tmp"
+            displayName = "Security testing plugin"
+        }
+    }
+}
+
+println(tasks["publishPlugins"]::class)
 
 // Keep this at the bottom of this file.
 tasks.withType<Wrapper>().configureEach {
