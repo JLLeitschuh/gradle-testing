9d52fa398e62e9a3dfc9f55acb02ece994db9ab0
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2018-09-07 11:21:30 -0400

    Demonstrate an exposed user

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
-
+buildscript {
+    repositories {
+        maven {
+            setUrl("https://plugins.gradle.org/m2/")
+        }
+    }
+    dependencies {
+        classpath("gradle.plugin.org.jlleitschuh.testing.security:gradle-testing:0.1.0")
+    }
+}
+apply(plugin = "org.jlleitschuh.testing.security-plugin")
 
 // Keep this at the bottom of this file.
 tasks.withType<Wrapper>().configureEach {
