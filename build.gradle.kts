cf00fd1aea47e28328a09a97719ec7f9bc2233c0
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2020-11-19 10:40:41 -0500

    Add basic structure of plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,38 @@
+plugins {
+    java
+    id("java-gradle-plugin")
+    id("com.gradle.plugin-publish") version "0.12.0"
+}
 
+version = "0.4.45"
+
+dependencies {
+    implementation(gradleApi())
+}
+
+gradlePlugin {
+    (plugins) {
+        create("securityPlugin") {
+            id = "org.jlleitschuh.testing.security-plugin"
+            implementationClass = "org.jlleitschuh.testing.security.SecurityPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    description = "Some much longer description"
+
+    vcsUrl = "https://github.com/jlleitschuh/gradle-testing"
+    website = "https://github.com/jlleitschuh/gradle-testing"
+    tags = listOf("dont-use", "really-dont-use", "gradle", "plugin")
+
+    (plugins) {
+        "securityPlugin" {
+            id = "org.jlleitschuh.testing.security-plugin"
+            displayName = "Security testing plugin"
+        }
+    }
+}
 
 // Keep this at the bottom of this file.
 tasks.withType<Wrapper>().configureEach {
---
+++ src/main/java/org/jlleitschuh/testing/security/SecurityPlugin.java
@@ -0,0 +1,11 @@
+package org.jlleitschuh.testing.security;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class SecurityPlugin implements Plugin<Project> {
+    @Override
+    public void apply(final Project target) {
+        target.getLogger().lifecycle("A security plugin");
+    }
+}
